c0b3ad147d33ed7d17c19786b23fa9ba1bddb45a
rafrex <code@rafrex.com>
2017-05-22 13:31:15 -0700

    Add react-component-dev boilerplate updated for react-fast-mount

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <div>Example App for React Fast Mount</div>
+      </div>
+    );
+  }
+}

a74cb30587262aa8aab48fca7933246f8bd99209
rafrex <code@rafrex.com>
2017-05-22 14:52:58 -0700

    Create fastMount higher order component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import React from 'react';
 
-export default class SomeComponent extends React.Component {
-  render() {
-    return (
-      <div>
-        Hello world from Some Component
-      </div>
-    );
-  }
+export default function fastMount(WrappedComponent, container) {
+  return class FastMount extends React.Component {
+    state = { renderComponent: false };
+    timerId = null;
+
+    componentDidMount() {
+      this.timerId = window.setTimeout(() => {
+        this.setState({ renderComponent: true });
+        this.timerId = null;
+      }, 16);
+    }
+
+    componentWillUnmount() {
+      if (this.timerId !== null) window.clearTimeout(this.timerId);
+    }
+
+    render() {
+      if (container !== undefined && React.isValidElement(container)) {
+        return React.cloneElement(
+          container,
+          {},
+          this.state.renderComponent === true
+            ? <WrappedComponent {...this.props} />
+            : null,
+        );
+      }
+      return this.state.renderComponent === true
+        ? <WrappedComponent {...this.props} />
+        : null;
+    }
+  };
 }
